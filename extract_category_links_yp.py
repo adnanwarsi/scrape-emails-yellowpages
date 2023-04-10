05c165af79997cf828cf1960b533206acc6b87b7
adnanwarsi <adnanwarsi@users.noreply.github.com>
2023-04-10 14:17:38 -0700

    Update extract_category_links_yp.py
    had ChatGPT (gpt4) refresh the code to write better. Have not tested it

--- extract_category_links_yp.py
+++ extract_category_links_yp.py
@@ -1,86 +1,105 @@
-from bs4 import BeautifulSoup
-import re
+"""
+Yellow Pages Scraper
+
+This script scrapes business listings from Yellow Pages based on a given category webpage.
+It extracts the links for individual businesses and saves them in a JSON file.
+
+Usage:
+    python3 yellow_pages_scraper.py {category_list_webpage}
+
+Example:
+    python3 yellow_pages_scraper.py https://www.yellowpages.com/san-diego-ca/auto-repair
+"""
+
+import sys
 import json
 import requests
-import csv
-import sys
+from bs4 import BeautifulSoup
 from urllib.parse import urlsplit
-from urllib.parse import urlparse
-from collections import deque
-import os
-
-# append the "?page=1" so on
-# website_category_listpage = 'https://www.yellowpages.com/san-diego-ca/auto-repair'
-# website_category_listpage = 'https://www.yellowpages.com/san-diego-ca/oil-change'
+from typing import List
 
 
-def print_usage():
-    print ('\nusage: \n\n>>>python3 ' + sys.argv[0] + '  {category lists webpage}\n\n')
-
-if len(sys.argv) < 2:
-    print_usage()
-    exit()
-
-website_category_listpage = sys.argv[1]
-
-# output file is the same name with extension .json
-json_out_file = website_category_listpage.split("/")[-1] + ".json"
+def print_usage() -> None:
+    """
+    Print the script usage message.
+    """
+    print(f'\nusage: \n\n>>>python3 {sys.argv[0]}  {{category lists webpage}}\n\n')
 
 
-def page_list_scan(webpage_url):
+def get_website_base(url: str) -> str:
+    """
+    Return the base URL for a given URL.
 
-    link_parts = urlsplit(webpage_url)
-    website_base = "{0.scheme}://{0.netloc}".format(link_parts)
+    Args:
+        url: The URL to get the base from.
 
-    biz_records = []
+    Returns:
+        The base URL as a string.
+    """
+    link_parts = urlsplit(url)
+    return f"{link_parts.scheme}://{link_parts.netloc}"
+
+
+def scan_page_list(webpage_url: str) -> List[str]:
+    """
+    Scan a Yellow Pages category webpage and extract business listing URLs.
+
+    Args:
+        webpage_url: The URL of the category webpage.
+
+    Returns:
+        A list of business listing URLs.
+    """
+    website_base = get_website_base(webpage_url)
+    business_records = []
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    }
 
     while True:
+        print(webpage_url)
+
         try:
-            print (webpage_url)
-
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
-            }
-
             response = requests.get(webpage_url, headers=headers, verify=True, timeout=10)
-            # print (response.text)
-
         except Exception as e:
-            print('FAILURE : unable to process website : ' + websitename + '\n' + str(e))
+            print(f'FAILURE : unable to process website : {webpage_url}\n{e}')
             break
 
         bsObj = BeautifulSoup(response.text, "html.parser")
 
         for result in bsObj.find_all('div', {'class': 'result'}):
-            # print ("FOUND : result")
             for vcard in result.find_all('div', {'class': 'v-card'}):
-                # print ('FOUND v-card')
-                for info in vcard.find('div', {'class': 'info'}):
-                     #print ("FOUND : info")
-                    if info.find('a', {'class': 'business-name'}):
-                        #print ('found business-name')
-                        bizlink = info.find('a', {'class': 'business-name'})['href']
+                info = vcard.find('div', {'class': 'info'})
 
-                        biz_link_parts = urlsplit(bizlink)
-                        biz_link_base = "{0.scheme}://{0.netloc}".format(biz_link_parts)
+                if info and info.find('a', {'class': 'business-name'}):
+                    business_link = info.find('a', {'class': 'business-name'})['href']
+                    business_link_base = get_website_base(business_link)
 
-                        if biz_link_base == "://":
-                            # print (website_base + bizlink)
-                            biz_records.append(website_base + bizlink)
+                    if business_link_base == "://":
+                        business_records.append(website_base + business_link)
 
-        # check if there is next page
+        # Check if there is a next page
         pagination = bsObj.find('div', {'class': 'pagination'})
-        if pagination.find('a', {'class':'next ajax-page'}):
-            webpage_url = website_base + pagination.find('a', {'class': 'next ajax-page'})['href']
+        next_page_link = pagination.find('a', {'class': 'next ajax-page'})
+        if next_page_link:
+            webpage_url = website_base + next_page_link['href']
         else:
             break
 
-    return biz_records
+    return business_records
 
 
-record_links = page_list_scan (website_category_listpage + '?page=1')
-print(json.dumps(record_links, indent=4))
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print_usage()
+        exit()
 
-with open(json_out_file, 'w') as outfile:
-    json.dump(record_links, outfile, indent=4, sort_keys=True)
-outfile.close()
+    website_category_listpage = sys.argv[1]
+    json_out_file = f"{website_category_listpage.split('/')[-1]}.json"
+
+    record_links = scan_page_list(website_category_listpage + '?page=1')
+    print(json.dumps(record_links, indent=4))
+
+    with open(json_out_file, 'w') as outfile:
+        json.dump(record_links, outfile, indent=4, sort_keys=True)
